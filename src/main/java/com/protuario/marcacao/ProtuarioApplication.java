a19fa05e56ad434f837ec6d6dd76eea7967891a0
Giovanna Marozzi <giovanna.machado99@hotmail.com>
2023-07-27 12:17:53 -0300

    Initializing

---
+++ src/main/java/com/protuario/marcacao/ProtuarioApplication.java
@@ -0,0 +1,13 @@
+package com.protuario.marcacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protuario/marcacao/ProtuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protuario.marcacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
